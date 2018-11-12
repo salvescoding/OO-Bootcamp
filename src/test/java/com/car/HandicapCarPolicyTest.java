ac13773237849ef62b84aaf565c70bc5f96173f6
salvescoding <salves2383@gmail.com>
2018-11-12 17:49:16 +0100

    [Sergio/Susan] Implement HandicapCarPolicy method applyPolicy with Unit tests

---
+++ src/test/java/com/car/HandicapCarPolicyTest.java
@@ -0,0 +1,39 @@
+package com.car;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class HandicapCarPolicyTest {
+
+    private HandicapCarPolicy handicapCarPolicy;
+
+    @BeforeEach
+    void setUp() {
+        handicapCarPolicy = new HandicapCarPolicy();
+    }
+
+    @Test
+    void shouldReturnCorrectParkingLotWhenOnlyOneParkingLotIsPassed() {
+        ParkingLot parkingLot = new ParkingLot(5);
+        Set<ParkingLot> lots = new HashSet<>(Arrays.asList(parkingLot));
+        assertEquals(parkingLot, handicapCarPolicy.applyPolicy(lots));
+    }
+
+    @Test
+    void shouldReturnCorrectParkingLotWhenThreeParkingLotsArePassed() {
+        ParkingLot parkingLot = new ParkingLot(5);
+        ParkingLot parkingLotSmall = new ParkingLot(1);
+        ParkingLot parkingLot100CapacityPercentage = new ParkingLot(1);
+        parkingLot100CapacityPercentage.park(new Car("Handicap"));
+        Set<ParkingLot> lots = new HashSet<>(Arrays.asList(parkingLot100CapacityPercentage,parkingLotSmall, parkingLot));
+        assertEquals(parkingLotSmall, handicapCarPolicy.applyPolicy(lots));
+
+    }
+}
